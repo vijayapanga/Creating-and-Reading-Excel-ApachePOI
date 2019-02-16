9a7188dfb15f61579978845261adf1faced1efe9
Vijaya Panga <VijayalakshmiPanga@gmail.com>
2019-02-16 14:10:28 +0000

    Creating and Reading Excel using Apache POI

---
+++ src/test/java/stepDefination/ExcelPOIAssignment.java
@@ -0,0 +1,67 @@
+package stepDefination;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ExcelPOIAssignment {
+
+    private static final String Excel_Name = "./src/test/java/utilities/Excelbook1.xlsx";
+
+    public static void main(String[] args) {
+
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Employee Data");
+        Object[][] EmpData = {
+                {"EmployeeNum", "EmployeeName", "Salary"},
+                {"101", "Emp101", 2100},
+                {"102", "Emp102", 2200},
+                {"103", "Emp103", 2300},
+                {"104", "Emp104", 2400},
+                {"105", "Emp105", 2500},
+
+        };
+
+
+        System.out.println("Creating and Printing excel");
+
+        int rowNum = 0;
+
+        for (Object[] EmpData1 : EmpData) {
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (Object field : EmpData1) {
+                Cell cell = row.createCell(colNum++);
+                if (field instanceof String)
+                {
+                    cell.setCellValue((String) field);
+                    System.out.print(cell.getStringCellValue()+" ");
+                } else if (field instanceof Integer)
+                {
+                    cell.setCellValue((Integer) field);
+                    System.out.print(cell.getNumericCellValue()+" ");
+                }
+            }
+            System.out.println();
+        }
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(Excel_Name);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+
+    }
+}
